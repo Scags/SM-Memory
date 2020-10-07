532468b3424a3352c1d4db221924c96c92a092a6
John Mascagni <johnmascagni@gmail.com>
2020-10-06 19:15:00 -0500

    Attempt at 64-bit support

--- natives.cpp
+++ natives.cpp
@@ -7,109 +7,181 @@ static cell_t Native_Calloc(IPluginContext *pContext, const cell_t *params)
 {
 	size_t num = (size_t)params[1];
 	size_t size = (size_t)params[2];
-	return (cell_t)calloc(num, size);
+
+	cell_t returnval;
+
+#ifdef PLATFORM_X86
+	returnval = (cell_t)calloc(num, size);
+#else
+	returnval = smutils->ToPseudoAddress(calloc(num, size));
+#endif
+	return returnval;
 }
 
 static cell_t Native_Malloc(IPluginContext *pContext, const cell_t *params)
 {
 	size_t size = (size_t)params[1];
-	return (cell_t)malloc(size);
+	cell_t returnval;
+
+#ifdef PLATFORM_X86
+	returnval = (cell_t)malloc(size);
+#else
+	returnval = smutils->ToPseudoAddress(malloc(size));
+#endif
+	return returnval;
 }
 
 #define VALID_MINIMUM_MEMORY_ADDRESS 0x10000 	// Yoinked from smn_core.cpp
 static cell_t Native_Realloc(IPluginContext *pContext, const cell_t *params)
 {
-	uintptr_t ptr = (uintptr_t)params[1];
-	if (ptr < VALID_MINIMUM_MEMORY_ADDRESS)
+#ifdef PLATFORM_X86
+	void *ptr = (void *)params[1];
+#else
+	void *ptr = smutils->FromPseudoAddress(params[1]);
+#endif
+	if ((uintptr_t)ptr < VALID_MINIMUM_MEMORY_ADDRESS)
 	{
 		return pContext->ThrowNativeError("Invalid address 0x%x is pointing to reserved memory.", ptr);
 	}
 
 	size_t size = (size_t)params[2];
-	SourceHook::SetMemAccess((void *)ptr, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
-	return (cell_t)realloc((void *)ptr, size);
+	SourceHook::SetMemAccess(ptr, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
+	cell_t returnval;
+
+#ifdef PLATFORM_X86
+	returnval = (cell_t)realloc(ptr, size);
+#else
+	returnval = smutils->ToPseudoAddress(realloc(ptr, size));
+#endif
+	return returnval;
 }
 
 static cell_t Native_Free(IPluginContext *pContext, const cell_t *params)
 {
-	uintptr_t ptr = (uintptr_t)params[1];
-	if (ptr < VALID_MINIMUM_MEMORY_ADDRESS)
+#ifdef PLATFORM_X86
+	void *ptr = (void *)params[1];
+#else
+	void *ptr = smutils->FromPseudoAddress(params[1]);
+#endif
+	if ((uintptr_t)ptr < VALID_MINIMUM_MEMORY_ADDRESS)
 	{
 		return pContext->ThrowNativeError("Invalid address 0x%x is pointing to reserved memory.", ptr);
 	}
 
-	SourceHook::SetMemAccess((void *)ptr, sizeof(void *), SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
-	free((void *)ptr);
+	SourceHook::SetMemAccess(ptr, sizeof(void *), SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
+	free(ptr);
 	return 0;
 }
 
 static cell_t Native_MemMove(IPluginContext *pContext, const cell_t *params)
 {
-	uintptr_t dest = (uintptr_t)params[1];
-	if (dest < VALID_MINIMUM_MEMORY_ADDRESS)
+#ifdef PLATFORM_X86
+	void *dest = (void *)params[1];
+#else
+	void *dest = smutils->FromPseudoAddress(params[1]);
+#endif
+	if ((uintptr_t)dest < VALID_MINIMUM_MEMORY_ADDRESS)
 	{
 		return pContext->ThrowNativeError("Invalid address 0x%x (arg 1) is pointing to reserved memory.", dest);
 	}
 
-	uintptr_t src = (uintptr_t)params[2];
-	if (src < VALID_MINIMUM_MEMORY_ADDRESS)
+#ifdef PLATFORM_X86
+	void *src = (void *)params[2];
+#else
+	void *src = smutils->FromPseudoAddress(params[2]);
+#endif
+	if ((uintptr_t)src < VALID_MINIMUM_MEMORY_ADDRESS)
 	{
 		return pContext->ThrowNativeError("Invalid address 0x%x (arg 2) is pointing to reserved memory.", src);
 	}
 
 	size_t size = (size_t)params[3];
-	SourceHook::SetMemAccess((void *)dest, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
-	SourceHook::SetMemAccess((void *)src, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
+	SourceHook::SetMemAccess(dest, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
+	SourceHook::SetMemAccess(src, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
 
-	return (cell_t)memmove((void *)dest, (void *)src, size);
+	cell_t returnval;
+
+#ifdef PLATFORM_X86
+	returnval = (cell_t)memmove(dest, src, size);
+#else
+	returnval = smutils->ToPseudoAddress(memmove(dest, src, size));
+#endif
+	return returnval;
 }
 
 static cell_t Native_MemCopy(IPluginContext *pContext, const cell_t *params)
 {
-	uintptr_t dest = (uintptr_t)params[1];
-	if (dest < VALID_MINIMUM_MEMORY_ADDRESS)
+#ifdef PLATFORM_X86
+	void *dest = (void *)params[1];
+#else
+	void *dest = smutils->FromPseudoAddress(params[1]);
+#endif
+	if ((uintptr_t)dest < VALID_MINIMUM_MEMORY_ADDRESS)
 	{
 		return pContext->ThrowNativeError("Invalid address 0x%x (arg 1) is pointing to reserved memory.", dest);
 	}
 
-	uintptr_t src = (uintptr_t)params[2];
-	if (src < VALID_MINIMUM_MEMORY_ADDRESS)
+#ifdef PLATFORM_X86
+	void *src = (void *)params[2];
+#else
+	void *src = smutils->FromPseudoAddress(params[2]);
+#endif
+	if ((uintptr_t)src < VALID_MINIMUM_MEMORY_ADDRESS)
 	{
 		return pContext->ThrowNativeError("Invalid address 0x%x (arg 2) is pointing to reserved memory.", src);
 	}
 
 	size_t size = (size_t)params[3];
-	SourceHook::SetMemAccess((void *)dest, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
-	SourceHook::SetMemAccess((void *)src, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
+	SourceHook::SetMemAccess(dest, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
+	SourceHook::SetMemAccess(src, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
 
-	return (cell_t)memcpy((void *)dest, (void *)src, size);
+	cell_t returnval;
+
+#ifdef PLATFORM_X86
+	returnval = (cell_t)memcpy(dest, src, size);
+#else
+	returnval = smutils->ToPseudoAddress(memcpy(dest, src, size));
+#endif
+	return returnval;
 }
 
 static cell_t Native_MemCmp(IPluginContext *pContext, const cell_t *params)
 {
-	uintptr_t ptr1 = (uintptr_t)params[1];
-	if (ptr1 < VALID_MINIMUM_MEMORY_ADDRESS)
+#ifdef PLATFORM_X86
+	void *ptr1 = (void *)params[1];
+#else
+	void *ptr1 = smutils->FromPseudoAddress(params[1]);
+#endif
+	if ((uintptr_t)ptr1 < VALID_MINIMUM_MEMORY_ADDRESS)
 	{
 		return pContext->ThrowNativeError("Invalid address 0x%x (arg 1) is pointing to reserved memory.", ptr1);
 	}
 
-	uintptr_t ptr2 = (uintptr_t)params[2];
-	if (ptr2 < VALID_MINIMUM_MEMORY_ADDRESS)
+#ifdef PLATFORM_X86
+	void *ptr2 = (void *)params[2];
+#else
+	void *ptr2 = smutils->FromPseudoAddress(params[2]);
+#endif
+	if ((uintptr_t)ptr2 < VALID_MINIMUM_MEMORY_ADDRESS)
 	{
 		return pContext->ThrowNativeError("Invalid address 0x%x (arg 2) is pointing to reserved memory.", ptr2);
 	}
 
 	size_t size = (size_t)params[3];
-	SourceHook::SetMemAccess((void *)ptr1, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
-	SourceHook::SetMemAccess((void *)ptr2, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
+	SourceHook::SetMemAccess(ptr1, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
+	SourceHook::SetMemAccess(ptr2, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
 
-	return (cell_t)memcmp((void *)ptr1, (void *)ptr2, size);
+	return (cell_t)memcmp(ptr1, ptr2, size);
 }
 
 static cell_t Native_MemSet(IPluginContext *pContext, const cell_t *params)
 {
-	uintptr_t ptr = (uintptr_t)params[1];
-	if (ptr < VALID_MINIMUM_MEMORY_ADDRESS)
+#ifdef PLATFORM_X86
+	void *ptr = (void *)params[1];
+#else
+	void *ptr = smutils->FromPseudoAddress(params[1]);
+#endif
+	if ((uintptr_t)ptr < VALID_MINIMUM_MEMORY_ADDRESS)
 	{
 		return pContext->ThrowNativeError("Invalid address 0x%x is pointing to reserved memory.", ptr);
 	}
@@ -117,8 +189,16 @@ static cell_t Native_MemSet(IPluginContext *pContext, const cell_t *params)
 	int val = (int)params[2];
 	size_t size = (size_t)params[3];
 
-	SourceHook::SetMemAccess((void *)ptr, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
-	return (cell_t)memset((void *)ptr, val, size);
+	SourceHook::SetMemAccess(ptr, size, SH_MEM_READ|SH_MEM_WRITE|SH_MEM_EXEC);
+
+	cell_t returnval;
+
+#ifdef PLATFORM_X86
+	returnval = (cell_t)memset(ptr, val, size);
+#else
+	returnval = smutils->ToPseudoAddress(memset(ptr, val, size));
+#endif
+	return returnval;
 }
 
 //static cell_t Native_AddressOf(IPluginContext *pContext, const cell_t *params)
