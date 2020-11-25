b68cce8c0b8cd4432340d61456c9aa4f2add4e3a
John Mascagni <johnmascagni@gmail.com>
2020-11-24 18:40:56 -0600

    Build scripts

---
+++ build/build.bat
@@ -0,0 +1,2 @@
+py ../configure.py --mms-path "/Users/johnm/OneDrive/Documents/alliedmodders/mmsource-1.10" --sm-path "/Users/johnm/OneDrive/Documents/alliedmodders/sourcemod" --sdks none
+ambuild
---
+++ build/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+python3 ../configure.py --mms-path "/mnt/c/Users/johnm/OneDrive/Documents/alliedmodders/mmsource-1.10" --sm-path "/mnt/c/Users/johnm/OneDrive/Documents/alliedmodders/sourcemod" --sdks none
+ambuild
